0e795587dcee0af8912d1639ee731c663a435ec1
dimiandre <dimiandre@gmail.com>
2023-04-06 16:15:56 +0200

    adjust chains

--- src/constants.ts
+++ src/constants.ts
@@ -5,22 +5,17 @@ export const CHAINS = [
     name: 'cosmoshub',
     chain_id: 'cosmoshub-4',
     prefix: 'cosmos',
-    voter_address: 'cosmos1sjllsnramtg3ewxqwwrwjxfgc4n4ef9u0tvx7u',
+    voter_address: 'cosmos1usvshtypjw57edkwxq3tagme665398f0japmst',
   },
-  { name: 'juno', chain_id: 'juno-1', prefix: 'juno', voter_address: 'juno1sjllsnramtg3ewxqwwrwjxfgc4n4ef9uee0aeq' },
-  { name: 'osmosis', chain_id: 'osmosis-1', prefix: 'osmo', voter_address: 'osmo1sjllsnramtg3ewxqwwrwjxfgc4n4ef9u8slkgw' },
-  { name: 'kava', chain_id: 'kava_2222-10', prefix: 'kava', voter_address: 'kava1ffcujj05v6220ccxa6qdnpz3j48ng024fqdz47' },
-  { name: 'agoric', chain_id: 'agoric-3', prefix: 'agoric', voter_address: 'agoric1z5xat323j3pzwln893zseksg8pavhred2vwpds' },
-  { name: 'irisnet', chain_id: 'irishub-1', prefix: 'iaa', voter_address: 'iaa1vk4kps6gala40ue4nltghym0vdq8z5ep308tve' },
-  {
-    name: 'persistence',
-    chain_id: 'core-1',
-    prefix: 'persistence',
-    voter_address: 'persistence1a8e70jds75rftrs9e5d2qmu4y38d3km7yxhns2',
-  },
-  { name: 'evmos', chain_id: 'evmos_9001-2', prefix: 'evmos', voter_address: 'evmos1tcty2k97cdv5sc6xcdf2atrrxq0zg7vvl2xq67' },
-  { name: 'regen', chain_id: 'regen-1', prefix: 'regen', voter_address: 'regen15qepg86xxesg7k633ent5t9vaweyf5zt5fhyuc' },
-  { name: 'sentinel', chain_id: 'sentinelhub-2', prefix: 'sent', voter_address: 'sent15nr70ed65h8gnwpn879u8pvtjcy7mt6cy4qatm' },
+  { name: 'juno', chain_id: 'juno-1', prefix: 'juno', voter_address: 'juno1juczud9nep06t0khghvm643hf9usw45r4v7lq6' },
+  { name: 'osmosis', chain_id: 'osmosis-1', prefix: 'osmo', voter_address: 'osmo1juczud9nep06t0khghvm643hf9usw45rt9w535' },
+  { name: 'crescent', chain_id: 'crescent-1', prefix: 'cre', voter_address: "cre1zegq2j52zh7g23k8dzfawwmz64j8d52uzand6e" },
+  { name: 'stride', chain_id: 'stride-1', prefix: 'stride', voter_address: "stride1crzkfx6tnm5zfnp3x5j3k62n3m0cw6ezlgzeca" },
+  { name: 'chihuahua', chain_id: 'chihuahua-1', prefix: 'chihuahua', voter_address: "chihuahua1t6t8ttmlzw5fgehx5wlfc02sefj5etwv9dylkw" },
+  { name: 'stargaze', chain_id: 'stargaze-1', prefix: 'stars', voter_address: "stars1s33zct2zhhaf60x4a90cpe9yquw99jj0qh8pdx" },
+  { name: 'comdex', chain_id: 'comdex-1', prefix: 'comdex', voter_address: "comdex1e64ezvwfy0tquprzum7e2aauxkqecnfpg0u222" },
+  { name: 'bitsong', chain_id: 'bitsong-2b', prefix: 'bitsong', voter_address: "bitsong1fkj2cn209yeexxyets98evrcmmds23hchtrdja" },
+  { name: 'desmos', chain_id: 'desmos-mainnet', prefix: 'desmos', voter_address: "desmos1u0xqasxgl6qkkckd860pt4xjqryxek4y3e66wg" }
 ];
 
 // Setup env
