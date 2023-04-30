e22673194fe5de8000b45f659b2697fef898b25f
dimiandre <dimiandre@gmail.com>
2023-04-30 14:08:25 +0200

    add cosmos vote transaction

--- src/constants.ts
+++ src/constants.ts
@@ -6,25 +6,28 @@ export const CHAINS = [
     chain_id: 'cosmoshub-4',
     prefix: 'cosmos',
     voter_address: 'cosmos1usvshtypjw57edkwxq3tagme665398f0japmst',
+    gasPrice: '0.0025uatom'
   },
-  { name: 'juno', chain_id: 'juno-1', prefix: 'juno', voter_address: 'juno1juczud9nep06t0khghvm643hf9usw45r4v7lq6' },
-  { name: 'osmosis', chain_id: 'osmosis-1', prefix: 'osmo', voter_address: 'osmo1juczud9nep06t0khghvm643hf9usw45rt9w535' },
-  { name: 'crescent', chain_id: 'crescent-1', prefix: 'cre', voter_address: 'cre1zegq2j52zh7g23k8dzfawwmz64j8d52uzand6e' },
-  { name: 'stride', chain_id: 'stride-1', prefix: 'stride', voter_address: 'stride1crzkfx6tnm5zfnp3x5j3k62n3m0cw6ezlgzeca' },
+  { name: 'juno', chain_id: 'juno-1', prefix: 'juno', voter_address: 'juno1juczud9nep06t0khghvm643hf9usw45r4v7lq6', gasPrice: '0.001ujuno' },
+  { name: 'osmosis', chain_id: 'osmosis-1', prefix: 'osmo', voter_address: 'osmo1juczud9nep06t0khghvm643hf9usw45rt9w535', gasPrice: '0.001uosmo' },
+  { name: 'crescent', chain_id: 'crescent-1', prefix: 'cre', voter_address: 'cre1zegq2j52zh7g23k8dzfawwmz64j8d52uzand6e', gasPrice: '0.001ucre' },
+  { name: 'stride', chain_id: 'stride-1', prefix: 'stride', voter_address: 'stride1crzkfx6tnm5zfnp3x5j3k62n3m0cw6ezlgzeca', gasPrice: '0.001ustrd' },
   {
     name: 'chihuahua',
     chain_id: 'chihuahua-1',
     prefix: 'chihuahua',
     voter_address: 'chihuahua1t6t8ttmlzw5fgehx5wlfc02sefj5etwv9dylkw',
+    gasPrice: '0.001uhuahua'
   },
-  { name: 'stargaze', chain_id: 'stargaze-1', prefix: 'stars', voter_address: 'stars1s33zct2zhhaf60x4a90cpe9yquw99jj0qh8pdx' },
-  { name: 'comdex', chain_id: 'comdex-1', prefix: 'comdex', voter_address: 'comdex1e64ezvwfy0tquprzum7e2aauxkqecnfpg0u222' },
-  { name: 'bitsong', chain_id: 'bitsong-2b', prefix: 'bitsong', voter_address: 'bitsong1fkj2cn209yeexxyets98evrcmmds23hchtrdja' },
+  { name: 'stargaze', chain_id: 'stargaze-1', prefix: 'stars', voter_address: 'stars1s33zct2zhhaf60x4a90cpe9yquw99jj0qh8pdx', gasPrice: '0.001ustars' },
+  { name: 'comdex', chain_id: 'comdex-1', prefix: 'comdex', voter_address: 'comdex1e64ezvwfy0tquprzum7e2aauxkqecnfpg0u222',  gasPrice: '0.001ucmdx' },
+  { name: 'bitsong', chain_id: 'bitsong-2b', prefix: 'bitsong', voter_address: 'bitsong1fkj2cn209yeexxyets98evrcmmds23hchtrdja',  gasPrice: '0.001ubtsg' },
   {
     name: 'desmos',
     chain_id: 'desmos-mainnet',
     prefix: 'desmos',
     voter_address: 'desmos1u0xqasxgl6qkkckd860pt4xjqryxek4y3e66wg',
+    gasPrice: '0.001udsm'
   },
 ];
 
@@ -40,7 +43,9 @@ export const cfg = {
   SlackSigningSecret: process.env.SLACK_SIGNING_SECRET || '',
   SlackAppToken: process.env.SLACK_APP_TOKEN || '',
   SlackChannelID: process.env.SLACK_CHANNEL_ID || '',
-  SlackAPIPort: process.env.SLAC_API_PORT || 3000,
+  SlackAllowedVoteUsers: process.env.SLACK_ALLOWED_VOTE_USERS.split(",") || [],
+
+  VotingWalletMnemonic: process.env.VOTING_WALLET_MNEMONIC || '',
 
   OpenAIAPIKey: process.env.OPENAI_API_KEY || '',
 };
--- src/slack.ts
+++ src/slack.ts
@@ -1,8 +1,11 @@
 import { cfg } from './constants';
 import { WebClient, LogLevel } from '@slack/web-api';
-import { Proposal } from 'entity/Proposal';
+import { Proposal } from './entity/Proposal';
 import { App, BlockAction, BlockElementAction, ButtonAction, SlackAction } from '@slack/bolt';
 import summarizeProposalDescription from './governance-ai';
+import { VoteOption } from 'cosmjs-types/cosmos/gov/v1beta1/gov';
+import { voteProposal, VoteRequest } from './cosmos-signer';
+import { AppDataSource } from './data-source';
 
 export const setupSlack = () => {
   // Setup slack bot
@@ -15,11 +18,18 @@ export const setupSlack = () => {
 
   slack.start();
 
-  console.log(`Slack API listeining on port ${cfg.SlackAPIPort}.`);
+  console.log(`Slack API listeining on socket.`);
 
   // Listen for vote button clicks and open a new modal asking for reason
   slack.action('vote', async ({ body, ack, say, client, logger }) => {
     let typed_body = <BlockAction>body;
+
+    // Check if user is allowed
+    if (cfg.SlackAllowedVoteUsers.indexOf(body.user.id) < 0) {
+      await say({text: `User ${typed_body.user.name} is not allowed to vote proposals!`, thread_ts: typed_body.message.ts})
+      await ack();
+      return
+    }
 
     // Get chain name and proposal id
     let [proposal_chain, proposal_id] = (<ButtonAction>typed_body.actions[0]).value.split("-")
@@ -55,24 +65,25 @@ export const setupSlack = () => {
             },
             {
               type: 'input',
-              block_id: 'input_c',
+              block_id: 'input_memo',
               label: {
                 type: 'plain_text',
                 text: `Write the reason of your vote on ${proposal_chain} proposal #${proposal_id}`
               },
               element: {
                 type: 'plain_text_input',
-                action_id: 'dreamy_input',
+                action_id: 'memo_action',
                 multiline: false
               }
             },
             {
               type: "input",
+              block_id: 'input_vote',
               element: {
                 type: "static_select",
                 placeholder: {
                   type: "plain_text",
-                  text: "Select an item",
+                  text: "Select a vote option",
                   emoji: true
                 },
                 options: [
@@ -108,7 +119,7 @@ export const setupSlack = () => {
                     value: "veto"
                   }
                 ],
-                action_id: "static_select-action"
+                action_id: "vote_action"
               },
               label: {
                 type: "plain_text",
@@ -142,19 +153,37 @@ export const setupSlack = () => {
 
     // Acknowledge action request
     await ack();
-    // await say({text: 'Request approved 👍', thread_ts: typed_body.message.ts});
   });
 
   // Listen for view submit, broadcast vote transaction with memo
   slack.view('vote_proposal', async ({ ack, body, view, client, logger }) => {
 
-    // todo: add vote logic
+    // unpack meta
+    let {proposal, ts} = JSON.parse(view.private_metadata);
 
-    logger.info(body)
+    // Get vote option & memo
+    const memo = view.state.values['input_memo']['memo_action'].value;
+    const vote_option = view.state.values['input_vote']['vote_action'].selected_option.value;
+
+    logger.info(memo)
+    logger.info(vote_option)
+
     // Acknowledge the view_submission request
     await ack();
-  });
+    
+    // Vote Proposal
+    const propRepo = AppDataSource.getRepository(Proposal);
+    const propDb = await propRepo.findOneBy({id: proposal.id});
 
+    voteProposal(propDb, {vote_option: voteInputToOption(vote_option), memo}).then(async (tx) => {
+      // send message in thread
+      await client.chat.postMessage({ text: `✅ Voted ${vote_option} on ${proposal.chain} proposal #${proposal.id}! Tx hash: ${tx.transactionHash}`, thread_ts: ts, channel: cfg.SlackChannelID })
+    }).catch(async (error) => {
+      logger.error(error)
+      await client.chat.postMessage({ text: `⚠ Error voting ${vote_option} on ${proposal.chain} proposal #${proposal.id}! ${error}`, thread_ts: ts, channel: cfg.SlackChannelID })
+    });
+
+  });
 };
 
 export const SendSlackNotification = async (proposal: Proposal) => {
@@ -235,3 +264,18 @@ export const SendSlackNotification = async (proposal: Proposal) => {
     throw error;
   }
 };
+
+export const voteInputToOption = (input: string): VoteOption => {
+  switch (input) {
+    case 'yes':
+      return VoteOption.VOTE_OPTION_YES;
+    case 'no':
+      return VoteOption.VOTE_OPTION_NO;
+    case 'abstain':
+      return VoteOption.VOTE_OPTION_ABSTAIN;
+    case 'veto':
+      return VoteOption.VOTE_OPTION_NO_WITH_VETO;
+    default:
+      return VoteOption.VOTE_OPTION_UNSPECIFIED;
+  }
+}
